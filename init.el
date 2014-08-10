a3f8a6c6f3e2ac1e175394e171fa04ecb81f5642
Aylons Hazzud <aylons@gmail.com>
2014-08-10 14:25:20 -0300

    Some org-mode adjustments

--- init.el
+++ init.el
@@ -1,1 +1,29 @@
 (require 'package)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-enabled-themes (quote (wheatgrass))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+(require 'org)
+(define-key global-map "\C-cl" 'org-store-link)
+(define-key global-map "\C-ca" 'org-agenda)
+(setq org-log-done t)
+
+;; Org-mode:
+
+(load "~/org-files.el")
+
+(add-to-list 'org-modules 'org-timer)
+(setq org-timer-default-timer 25)
+
+(add-hook 'org-clock-in-hook (lambda ()
+      (if (not org-timer-current-timer) 
+      (org-timer-set-timer '(16)))))
