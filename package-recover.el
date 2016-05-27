5088bea54f71b634031f90d2579e9b1e11be7bac
Aylons Hazzud <gustavo@aylons.com.br>
2016-05-27 02:12:07 -0300

    Recover.el identation

--- package-recover.el
+++ package-recover.el
@@ -1,21 +1,19 @@
 ;; Saves list of installed packages so they do not need to be copied over git
 
-(require 'package)
-
 (defun write-package-list (package-file)
   (with-temp-file package-file
-	(insert (format "%s" package-activated-list))))
+    (insert (format "%s" package-activated-list))))
 
 (defun read-package-list (package-file)
   (split-string
-  (with-temp-buffer
-	(insert-file-contents file)
+   (with-temp-buffer
+    (insert-file-contents package-file)
 	(buffer-substring-no-properties
-	 (+ 1 (point-min))					; +1 to remove parenthesis
-	 (- (point-max) 1)))))				; -1 to remove parenthesis
+	 (+ 1 (point-min))		; +1 to remove parenthesis
+	 (- (point-max) 1)))))		; -1 to remove parenthesis
 
 (defun recover-packages (package-file)
-	(mapc 'package-install
-		  (mapcar 'intern
-				  (read-package-list package-file))))
-	
+  (mapc 'package-install
+	(mapcar 'intern
+		(read-package-list package-file))))
+
