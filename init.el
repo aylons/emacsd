28c2f7a5922bb575f8bbbc9aeda38d8df25d5608
Aylons Hazzud <aylons@aylons.com.br>
2018-09-09 18:47:11 -0300

    Customized helm delay to avoid "Window not ready"

--- init.el
+++ init.el
@@ -5,6 +5,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-enabled-themes (quote (wheatgrass)))
+ '(helm-exit-idle-delay 0.0)
  '(inhibit-startup-screen t)
  '(magit-log-arguments (quote ("--graph" "--color" "--decorate" "-n256")))
  '(org-file-apps
